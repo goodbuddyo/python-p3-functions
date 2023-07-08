47893ba44845da3203ebab92d43ff974890a8b44
Seano <sean01234@yahoo.com>
2023-07-08 01:39:27 -0400

    4 out of 5 passing

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,33 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    # pass
+    return print("Hello, programmer!") 
+greet_programmer()
 
 def greet(name):
-    pass
+    # pass
+    return print(f"Hello, {name}!")
+greet("Naureen")
 
 def greet_with_default(name="programmer"):
-    pass
+    return print(f"Hello, {name}!")
+greet_with_default("Sunny")
+    
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+add(1, 2)
 
 def halve(number):
-    pass
+    #if not isinstance(number, int) or not isinstance(number, float):
+    #    pass
+    #elif isinstance(number, int) or isinstance(number, float):
+    #    print(number / 2)
+    if type(number) is float or type(number) is int:
+        print(number / 2)
+    else:
+        pass
+
+
+
